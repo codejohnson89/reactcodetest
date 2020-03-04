6823ddce06b12b34cb7352efbd53f861d75f76d5
Micah Johnson <mjohnson@teleflora.org>
2020-03-03 23:44:16 -0600

    Finsihed test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+import axios from 'axios';
+
 function App() {
+  const [selected, setSelected] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then(res => {
+      setSelected(res.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Names below</div>
+      <input
+        type="text"
+        value={search}
+        onChange={e => {
+          e.preventDefault();
+          setSearch(e.target.value);
+        }}
+      />
+      {/* Grabs the names first of each object then searches for anything matching the search results */}
+      {selected.filter(select => select.name.toLowerCase().includes(search), selected)
+          .map(select => <div className="list">{select.name}</div>)}
+          {/* Chaining the .map function to display the remaining names that includes the text in the input field  */}
     </div>
   );
 }
